8b666452f2d457048c6dd9cbdd175098d3baec2f
dan <dan@localhost.localdomain>
2019-11-23 10:24:20 +0300

    initialize our sqlachemy extension and create its instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,22 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap 
 from config import config_options
+from flask_sqlalchemy import SQLALchemy
 
 
 bootstrap = Bootstrap()
+db = SQLALchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
 
     #creating the app configuration
     app.config.from_object(config_options[config_name])
+
+
     # intilizing flask extensions
     bootstrap.init_app(app)
+    db.init_app(app)
     
     # configure uploadset
 
