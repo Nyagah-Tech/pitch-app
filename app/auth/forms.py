dd0ae7be44ba5379c0037c2f34eaef0ddc762de9
dan <dan@localhost.localdomain>
2019-11-23 13:55:33 +0300

    create the registration form class with validators kept in place

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,3 +2,10 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
+
+class RegForm(FlaskForm):
+    email = StringField('Your Email Address ...',validators=[Required(),Email()])
+    username = StringField('Enter username...',validators=[Required()])
+    password = PasswordField('Password...',validators=[Required(),EqualTo('pass_confirm',message = 'password must match')])
+    pass_confirm = PasswordField('Confirm password',validators=[Required()])
+    submit = SubmitField('Sign Up')
