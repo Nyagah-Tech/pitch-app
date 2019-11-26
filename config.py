a8308841c92077dc8bdc8651f6a2d217029aaff7
dan <dan@localhost.localdomain>
2019-11-26 08:51:39 +0300

    final app

--- config.py
+++ config.py
@@ -7,6 +7,15 @@ class Config:
     SECRET_KEY ='dm/01254'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://dan:12345@localhost/pitch'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #email configuration
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME ='danmuv12@gmail.com'
+    MAIL_PASSWORD ='Dm17/01254'
+
+
     pass
 
 class ProdConfig (Config):
